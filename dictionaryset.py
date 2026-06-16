87521337a4fbc4664b59c4f9f62518dc1ff6c822
vinayhooda83 <vinayhooda83@gmail.com>
2026-06-16 22:19:24 +0530

    a file about dictionary

---
+++ dictionaryset.py
@@ -0,0 +1,31 @@
+#dictionary 
+ 
+my_dic={"name": "vinay", "age": 21, "city": "rohtak"}
+print(my_dic)
+
+# # to access key value
+# print(my_dic["name"])
+
+# # to modify key value
+# my_dic["age"]=22
+# print(my_dic)
+
+# # to adding key value
+# my_dic["country"]="India"
+# print(my_dic)
+
+# # to remove key value 
+# my_dic.pop("country")
+# print(my_dic)
+
+# # to find lenght 
+# print(len(my_dic))
+
+# #to show all key
+# print(my_dic.keys())
+
+#to show all values
+print(my_dic.values())
+
+# to 
+print(my_dic.items())
