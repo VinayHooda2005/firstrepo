be3f99c8b0a2c10f06bb7bcabf2a0e5079a7b349
vinayhooda83 <vinayhooda83@gmail.com>
2026-06-23 21:16:11 +0530

    nested loops pattern printing

--- nestedloop.py
+++ nestedloop.py
@@ -4,17 +4,18 @@ for i in range(1, 6):
         print("*",end=" ")
     print()
 
-print()
-print()
 
-# right triangle pattern
+print()
+# inverted right triangle pattern
 for i in range(5,0,-1):
     for j in range(i):
         print("*",end=" ")
     print()
 
-# inverted right triangle pattern
-for i in range(0,6,): 
+
+print()
+# right triangle pattern
+for i in range(1,6): 
     for j in range(i):
         print("*",end=" ")
     print()
