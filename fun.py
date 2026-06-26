de78ca1513918ca2109c3af63dcd7b735e651675
vinayhooda83 <vinayhooda83@gmail.com>
2026-06-26 22:44:45 +0530

    function practice question

---
+++ fun.py
@@ -0,0 +1,39 @@
+# addition of two number
+def addtion(a,b):
+    return a+b
+
+print(addtion(5,3))
+print(addtion(10,20))
+
+
+
+# greet of student
+def greet():
+    print("hello ,students!")
+greet()
+
+
+
+# print name
+def my_name(name):
+    return name
+print(my_name("Vinay"))
+
+
+# check even or odd
+def check_even_odd(num):
+    if num%2==0:
+        print(num,"num is even")
+    else:
+        print(num,"num is odd")
+
+    
+check_even_odd(69)
+check_even_odd(10)
+
+
+# square of number
+def square(a):
+    sqr=a*a
+    print(sqr)
+square(9)
