22f84c882db2f5fa39087a4d06cae6489b656026
VinayHooda2005 <vinayhooda2005@gmail.com>
2026-07-14 20:44:36 +0530

    basic practice question

---
+++ practice1.py
@@ -0,0 +1,87 @@
+name="Vinay Hooda"
+print(name)
+
+age=21
+print(age)
+
+a=12
+b=14
+sum=a+b
+print(sum)
+
+a=12
+b=14
+print("before swap ","a=",a,"b=",b)
+a,b=b,a
+print("afte swap ","a=",a,"b=",b)
+print
+
+a="crd"
+print(type(a))
+
+b=int("12")
+print(type(b))
+
+c=str(100)
+print(type(c))
+
+height=float(180)
+age=21
+print("height=",height,"age=",age)
+
+length=12
+breadth=14
+area=2*(length+breadth)
+print("area of rectangle=",area)
+
+r=10
+pi=3.14
+area=pi*r*r
+print("area of circle=",area)
+
+
+p=2000
+r=10
+t=2
+SI=(p*r*t)/100
+print("simple interest=",SI)
+
+a=12
+b=14
+c=16
+avg=(a+b+c)/3
+print("average of three=",avg)
+
+a=12
+square=a*a
+cube=a*a*a
+print("square of a=",square)
+print("cube of a=",cube)
+
+
+num=12
+if num%2==0:
+    print("even")
+else:
+    print("odd")
+
+km=12
+m=km*1000
+print("in ",km,"km there are",m,"m")
+
+num=12
+f=(1.8*num)+32
+print("fahrenheit=",f)
+
+unit=12
+rate=4
+bill=unit*rate
+print("electricity bill=",bill)
+
+
+dividend = 25
+divisor = 4
+quotient = dividend // divisor
+remainder = dividend - (quotient * divisor)
+print("quotient:", quotient)
+print("remainder:", remainder)
